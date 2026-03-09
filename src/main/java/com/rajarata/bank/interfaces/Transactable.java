516af687b01126df045ceb84114759555c20e95b
SITH <ranaweerasithuruwan@gmail.com>
2026-03-09 17:06:10 +0530

    Fix package declarations in Customer, Account, Transactable and DataStore

--- src/main/java/com/rajarata/bank/interfaces/Transactable.java
+++ src/main/java/com/rajarata/bank/interfaces/Transactable.java
@@ -3,4 +3,7 @@
 public interface Transactable {
     void deposit(double amount);
     void withdraw(double amount);
+    boolean transfer(Transactable targetAccount, double amount);
+    double getBalance();
+
 }
