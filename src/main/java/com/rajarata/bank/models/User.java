1de2c9ddb93156a6c6d0958116ab9a11b776f3c0
SITH <ranaweerasithuruwan@gmail.com>
2026-03-19 00:19:08 +0530

    Update Customer class with inheritance, composition, and notification features

--- src/main/java/com/rajarata/bank/models/User.java
+++ src/main/java/com/rajarata/bank/models/User.java
@@ -2,10 +2,10 @@
 
 import java.time.LocalDateTime;
 
-public abstract class User {
+public abstract class User { 
     // Encapsulation: private fields with getters/setters
 
-    private String Id;
+    private String id;
     private String name;
     private String email;
     private String password;
@@ -14,28 +14,33 @@ public abstract class User {
     private boolean isActive;
 
     // Constructor
-    protected User(String name, String email, String password, String role, String Id, LocalDateTime createdAt, boolean isActive) {
+    protected User(String name, String email, String password, String role, String id, LocalDateTime createdAt, boolean isActive) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.role = role;
-        this.Id = Id;
+        this.id = id;
         this.createdAt = createdAt;
         this.isActive = isActive;
     }
 
     public String getName() { return name; }
-        public void setName(String name) { this.name = name; }
+        public void setName(String name) {
+            if (name != null && !name.isBlank()) {
+            } this.name = name; }
 
     public String getEmail() { return email; }
-        public void setEmail(String email) { this.email = email; }
+        public void setEmail(String email) {
+            if (email != null && !email.contains("@")) {
+            } this.email = email; }
 
     public String getPassword() { return password; }
-        public void setPassword(String password) { this.password = password; }
+        public void setPassword(String password) { 
+     if (password != null && !password.isBlank()) this.password = password; }
 
-    public String getId() { return Id; }
+    public String getId() { return id; }
 
-        public LocalDateTime getCreatedAt() { return createdAt; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
     public boolean isActive() { return isActive; }
     public void setActive(boolean isActive) { this.isActive = isActive; }
     
